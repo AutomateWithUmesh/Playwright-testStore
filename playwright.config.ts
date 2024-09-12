e8d9745c28fc2f22886ce83fd9c5f871d3fd099f
codewithumesh <umeshd25@gmail.com>
2024-09-12 16:45:26 +0530

    added page manager class

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: false,
+  fullyParallel: false, //if true, then tests within a file will run parallely
+  workers: process.env.CI ? 1 : undefined, 
+  
+ // workers are new instance of the webbrowser.
+ // By default, playwright creates workers for each spec file and executes them in parallel
+ // However, tests inside each spec file will be executed sequentially if fullyParallel: false
+ // Playwright selected test spec files randomly
+ //
+
   retries: 0,
   reporter: 'html',
   
